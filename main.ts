a7ea4410a72a39e6cd69af3d76881e3a87779149
Jolo2K <74709762+Jolo2K@users.noreply.github.com>
2021-03-09 15:58:08 +0100

    Uppdatera pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,3 @@
-let mit_val = 0
-let bits_val = 0
 input.onButtonPressed(Button.A, function () {
     mit_val = 1
     basic.showLeds(`
@@ -15,9 +13,16 @@ input.onButtonPressed(Button.A, function () {
         basic.showIcon(IconNames.Meh)
     } else if (bits_val == 2) {
         basic.showIcon(IconNames.Happy)
+        resultat_jag = 1 + resultat_jag
     } else {
         basic.showIcon(IconNames.Skull)
+        resultat_bot = 1 + resultat_bot
     }
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("" + (resultat_jag))
+    basic.showString("-")
+    basic.showString("" + (resultat_bot))
 })
 input.onButtonPressed(Button.AB, function () {
     mit_val = 3
@@ -34,8 +39,10 @@ input.onButtonPressed(Button.AB, function () {
         basic.showIcon(IconNames.Meh)
     } else if (bits_val == 1) {
         basic.showIcon(IconNames.Happy)
+        resultat_jag = 1 + resultat_jag
     } else {
         basic.showIcon(IconNames.Skull)
+        resultat_bot = 1 + resultat_bot
     }
 })
 input.onButtonPressed(Button.B, function () {
@@ -53,7 +60,29 @@ input.onButtonPressed(Button.B, function () {
         basic.showIcon(IconNames.Meh)
     } else if (bits_val == 3) {
         basic.showIcon(IconNames.Happy)
+        resultat_jag = 1 + resultat_jag
     } else {
         basic.showIcon(IconNames.Skull)
+        resultat_bot = 1 + resultat_bot
+    }
+})
+let bits_val = 0
+let mit_val = 0
+let resultat_bot = 0
+let resultat_jag = 0
+resultat_jag = 0
+resultat_bot = 0
+basic.forever(function () {
+    if (resultat_jag == 3) {
+        music.playMelody("C D E F G A B C5 ", 277)
+        basic.showString("Du vann!")
+        resultat_bot = 0
+        resultat_jag = 0
+    }
+    if (resultat_bot == 3) {
+        music.playMelody("C5 B A G F E D C ", 274)
+        basic.showString("Du förlora!")
+        resultat_bot = 0
+        resultat_jag = 0
     }
 })
